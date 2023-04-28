b0a04c0793947206c2950aea1c5b4079db1fae30
rntvilela <renatovilela55@gmail.com>
2023-04-28 17:38:50 -0300

    Create the files index, scripts and styles and the functions add, sub, mul, div and operate.

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+const add = function (a, b) {
+	return a + b;
+}
+
+const sub = function (a, b) {
+	return a - b;
+}
+
+const mul = function (a, b) {
+	return a * b;
+}
+
+const div = function (a, b) {
+	return a / b;
+}
+
+const operate = function (operator, first_number, second_number) {
+	let result = 0;
+
+	if (operator === '+') result = add(first_number, second_number);
+	else if (operator === '-') result = sub(first_number, second_number);
+	else if (operator === '*') result = mul(first_number, second_number);
+	else if (operator === '/') result = div(first_number, second_number);
+
+	return Number(result.toFixed(2));
+}
